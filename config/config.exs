c69205947a34eb4bb1826c7da0577d57d9a1dabc
Alan Andrade <alan.andradec@gmail.com>
2015-11-27 21:44:33 -0800

    update

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,8 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
+config :doc,
+  main: "Curriculum",
+  source_url: "http://elixirbridge.github.io/doc/",
+  extras: ["README.md"]
