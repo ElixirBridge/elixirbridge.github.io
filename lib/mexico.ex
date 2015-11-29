9ca1933665902ae01761cb4bcc557f25c073c036
Alan Andrade <alan.andradec@gmail.com>
2015-11-29 00:37:06 -0800

    [WIP] Mexico

--- lib/mexico.ex
+++ lib/mexico.ex
@@ -1,5 +1,14 @@
 defmodule Mexico do
   @moduledoc """
   # Welcome to Mexico
+
+  Fly to Cancun. Work with a tourism business and find out the right number
+  per year of tourists they need. They have a semi working system you have
+  to tweak. There seem to be a problem with counter of tourists.
+
+      # The excercise will have a Module with the "legacy" code from the
+      # problem loaded in the env. The problem requires you to figure out what's the bug. Few
+      # adjustemnts in the source code should take place. Finish by having the user create
+      # his first function.
   """
 end
